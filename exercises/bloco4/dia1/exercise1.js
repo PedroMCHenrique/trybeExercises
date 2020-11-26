010cb27acceb2cfc558785e5dd60c331d5f65277
Pedro Henrique <pedro.876.ph@gmail.com>
2020-11-25 23:18:32 -0300

    exercicio 4 feito

--- exercises/bloco4/dia1/exercise1.js
+++ exercises/bloco4/dia1/exercise1.js
@@ -38,3 +38,14 @@ if (e > f && e > g){
 } else {
     console.log('O maior número é '+ g);
 }
+
+//Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+let h = 0;
+if (h > 0){
+    console.log('positive');
+} else if (h < 0) {
+    console.log('negative');
+} else {
+    console.log('zero');
+}
