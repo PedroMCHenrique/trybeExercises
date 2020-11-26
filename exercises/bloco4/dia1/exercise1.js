8d9181b8bd30843707351db92d0b40e5cfce8967
Pedro Henrique <pedro.876.ph@gmail.com>
2020-11-25 22:24:23 -0300

    exercicios 1 e 2 feitos

---
+++ exercises/bloco4/dia1/exercise1.js
@@ -0,0 +1,26 @@
+//Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+//Adição
+//Subtração
+//Multiplicação
+//Divisão
+//Módulo
+
+let a = 5;
+let b = 2;
+
+console.log('A soma é ' +(a + b));
+console.log('A subtração é ' +(a - b));
+console.log('A multiplicação é '+(a * b));
+console.log('A divisão é ' +(a / b));
+console.log('O módulo é ' +(a % b));
+
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let c = 21
+let d = 25
+
+if (c > d){
+    console.log(c + ' é maior que '+ d);
+} else {
+    console.log(d + ' é maior que ' + c)
+}
